8ecc2faa01639a460ed74e9a9e9ca3724f0a4226
PauloBarco <paulo.barco34@gmail.com>
2026-04-27 17:36:04 -0300

    feat: test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from "../generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
-import pg from "pg"
 
-const connectionString = process.env.DATABASE_URL || ""
-
-const adapter = new PrismaPg(connectionString)
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL || "",
+})
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
