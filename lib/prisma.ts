f8491ec0ea5f03c9151a8f1311f30c2b22dc1df8
PauloBarco <paulo.barco34@gmail.com>
2026-04-27 17:35:49 -0300

    feat: inicialiar

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,20 +2,9 @@ import { PrismaClient } from "../generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 import pg from "pg"
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  connectionTimeoutMillis: 15000,
-  idleTimeoutMillis: 60000,
-  max: 5,
-  idle_in_session_timeout: 60000,
-  keepalives: 1,
-  keepalives_idle: 30,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-})
+const connectionString = process.env.DATABASE_URL || ""
 
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg(connectionString)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
